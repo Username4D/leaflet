ca85c428216cc2d5b68afabdbab9132c8935a4b8
Username4D <studio@kremzow.org>
2025-10-23 17:10:07 +0200

    feat: Switched to threading to make downloading async

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-from pickle import FRAME
-from typing import Text
 import customtkinter as ctk
 import subprocess as sp
+import threading
+
+
+# Setup
 
 yt_dlp_path = "C:/Program Files/yt-dlp.exe"
 output_path = "%USERPROFILE%/Videos"
+
+active_threads = [] # All active download threads
+
 # Download function
+
 def download(url = "yt"):
-    sp.run([yt_dlp_path, url, "-P", output_path], capture_output=True)
-    
+    print("Started download of " + url)
+    output = sp.run([yt_dlp_path, url, "-P", output_path], capture_output=True, text=True)
+    for line in output.stdout:
+        print(line, end="")
 
-
+def create_download_thread(url = "yt"):
+    download_thread = threading.Thread(target=download, kwargs={"url": url})
+    active_threads.append(download_thread)
+    download_thread.start()
 # Window
 window = ctk.CTk()
 window.geometry("200x200")
@@ -26,7 +37,7 @@ url_bar = ctk.CTkEntry(master=download_area, textvariable=current_url)
 url_bar.pack()
 
 # Download button
-download_button = ctk.CTkButton(master=download_area, text="download", command=lambda: download(url=current_url.get()))
+download_button = ctk.CTkButton(master=download_area, text="download", command=lambda: create_download_thread(url=current_url.get()))
 download_button.pack()
 
 download_area.pack()
