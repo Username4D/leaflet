9b29817573143426e4b06bdf016a3cbbad46e8d7
Username4D <studio@kremzow.org>
2025-10-26 12:05:41 +0100

    feat: icon manager script

---
+++ ui/icons.py
@@ -0,0 +1,7 @@
+import customtkinter as ctk
+from PIL import Image
+
+# All Icons
+download = ctk.CTkImage(light_image=Image.open("assets/download-1.png"), size=(20,20))
+block = ctk.CTkImage(light_image=Image.open("assets/block.png"), size=(40,40))
+settings = ctk.CTkImage(light_image=Image.open("assets/settings-1.png"), size=(40,40))
